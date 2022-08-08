01df81cdee8be168d62dcbdb0036603ba76cadf4
Wesley Matos <wm4tos.777@gmail.com>
2022-08-08 07:14:11 -0300

    feat: listen to join-room event on io connection

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import express from "express";
 import { Server } from "http";
 import { Server as SocketIoServer } from "socket.io";
@@ -8,5 +9,11 @@ const app = express();
 const server = new Server(app);
 
 const io = new SocketIoServer(server);
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log('{ roomId, userId } :>> ', { roomId, userId });
+  });
+});
 
 server.listen(PORT);
