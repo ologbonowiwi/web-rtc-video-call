3cf32e5e80cb314ba4b2a0346f4c1ee37cff4e12
Wesley Matos <wm4tos.777@gmail.com>
2022-08-08 08:08:16 -0300

    feat: emit 'join-room' event when open peer

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
 const socket = io(); // io is a global variable
 
-socket.emit('join-room', ROOM_ID, 10);
+// Peer is a global variable
+const myPeer = new Peer(undefined, { // undefined is a random ID
+  host: '/',
+  port: '3001',
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+})
+
