88e1813f3cb966ea9c00d82f635862457061a553
Wesley Matos <wm4tos.777@gmail.com>
2022-08-08 09:19:20 -0300

    feat: emit disconnected event

--- src/server.js
+++ src/server.js
@@ -28,6 +28,10 @@ io.on('connection', socket => {
     socket.join(roomId);
 
     socket.to(roomId).emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId);
+    })
   });
 });
 
