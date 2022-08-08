9a590a6c53a565feec194dfcbc825f819cd6740f
Wesley Matos <wm4tos.777@gmail.com>
2022-08-08 08:09:38 -0300

    feat: render my camera input on screen

--- public/script.js
+++ public/script.js
@@ -1,4 +1,20 @@
 const socket = io(); // io is a global variable
+const videoGrid = document.getElementById('video-grid');
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+  addVideoStream(myVideo, stream)
+})
 
 // Peer is a global variable
 const myPeer = new Peer(undefined, { // undefined is a random ID
