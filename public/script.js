5c595d30023e2eb82911d28b65a38c7c89eca0c2
Wesley Matos <wm4tos.777@gmail.com>
2022-08-10 18:30:36 -0300

    feat: add and remove connections

--- public/script.js
+++ public/script.js
@@ -4,6 +4,8 @@ const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+const peers = {};
+
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
@@ -14,9 +16,40 @@ const addVideoStream = (video, stream) => {
 
 const processStream = stream => {
   addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+
+    const video = document.createElement('video');
+
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    })
+  });
 }
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(processStream)
+socket.on('user-disconnected', userId => {
+  peers[userId]?.close?.();
+})
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  socket.on('user-connected', (userId) => {
+    setTimeout(connectToNewUser, 1000, userId, stream);
+    // connectToNewUser(userId, stream);
+  });
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+
+    const video = document.createElement('video');
+
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    })
+  });
+})
 
 // Peer is a global variable
 const myPeer = new Peer(undefined, { // undefined is a random ID
@@ -27,4 +60,21 @@ const myPeer = new Peer(undefined, { // undefined is a random ID
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 })
+
+const connectToNewUser = (userId, stream) => {
+  const call = myPeer.call(userId, stream);
+
+  const video = document.createElement('video');
+  video.setAttribute('id', userId);
+
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  call.on('close', () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
 
